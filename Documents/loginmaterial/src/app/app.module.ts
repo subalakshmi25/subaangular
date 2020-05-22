52f143fb70881d67b5338b5154fde24f6d63b60b
subav <vijayakumar.vij09@gmail.com>
2020-05-22 15:56:36 +0530

    login page create with angular material

---
+++ Documents/loginmaterial/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
